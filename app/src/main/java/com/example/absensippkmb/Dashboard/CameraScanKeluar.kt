889ab40438566e5e6985692ece7a737c83176d62
fahmipradana09 <fahmipradama@gmail.com>
2022-08-11 23:33:40 +0700

    first Commit

---
+++ app/src/main/java/com/example/absensippkmb/Dashboard/CameraScanKeluar.kt
@@ -0,0 +1,12 @@
+package com.example.absensippkmb.Dashboard
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.absensippkmb.R
+
+class CameraScanKeluar : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_camerascankeluar)
+    }
+}
